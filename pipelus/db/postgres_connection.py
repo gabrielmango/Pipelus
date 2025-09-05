6b1915a735c9572d5103f9b2040fe3a1ac318e2a
gabrielhmango <gabrielhmango@gmail.com>
2025-09-05 15:31:47 -0300

    refaxtor(db): implementa AsyncBaseConnection  na classe AsyncPostgresConnection

--- pipelus/db/postgres_connection.py
+++ pipelus/db/postgres_connection.py
@@ -7,7 +7,8 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import (AsyncConnection, AsyncEngine,
                                     create_async_engine)
 
-from pipelus.db.base_connection import SyncBaseConnectionWithExecute
+from pipelus.db.base_connection import (AsyncBaseConnection,
+                                        SyncBaseConnectionWithExecute)
 
 
 class PostgresConnection(SyncBaseConnectionWithExecute):
@@ -53,48 +54,26 @@ class PostgresConnection(SyncBaseConnectionWithExecute):
             return False
 
 
-class AsyncPostgresConnection:
+class AsyncPostgresConnection(AsyncBaseConnection):
     """Gerencia a conexão assíncrona com um banco PostgreSQL."""
 
     def __init__(self, connection_string: str) -> None:
         """Inicializa a classe AsyncPostgresConnection."""
-        self.connection_string: str = connection_string
+        super().__init__(connection_string)
         self.engine: AsyncEngine = create_async_engine(
-            self.connection_string, echo=False
+            self.connection_string, echo=False, future=True
         )
-        self.connection: Optional[AsyncConnection] = None
 
-    async def __aenter__(self) -> 'AsyncPostgresConnection':
-        """Abre a conexão assíncrona com o PostgreSQL ao entrar no contexto."""
-        try:
-            logging.info('Abrindo conexão com PostgreSQL...')
-            self.connection = await self.engine.connect()
-            logging.info('Conexão estabelecida com PostgreSQL.')
-            return self
-        except Exception as e:
-            logging.error(f'Erro ao conectar ao PostgreSQL: {str(e)}')
-            raise
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
-        """Fecha a conexão ao sair do contexto."""
-        if self.connection:
-            try:
-                await self.connection.close()
-                logging.info('Conexão com PostgreSQL encerrada com sucesso.')
-            except Exception as e:
-                logging.error(
-                    f'Erro ao fechar conexão com PostgreSQL: {str(e)}'
-                )
-
-    async def execute_query(
-        self, query: str, params: Optional[Dict[str, Any]] = None
-    ) -> List[Dict[str, Any]]:
+    async def execute_query(self, query: str) -> List[Dict[str, Any]]:
         """Executa uma query de leitura (SELECT) e retorna os resultados."""
+        if not self.connection:
+            logging.error("Conexão não está aberta. Use 'async with'.")
+
         try:
-            logging.debug('Executando query.')
-            result = await self.connection.execute(text(query), params or {})
+            logging.debug('Executando query assíncrona no PostgreSQL.')
+            result = await self.connection.execute(text(query))
             columns = result.keys()
-            data = [dict(zip(columns, row)) for row in await result.fetchall()]
+            data = [dict(zip(columns, row)) async for row in result]
             logging.info(
                 f'Query executada com sucesso. Linhas retornadas: {len(data)}'
             )
@@ -103,19 +82,18 @@ class AsyncPostgresConnection:
             logging.error(f'Erro ao executar query: {str(e)}')
             return []
 
-    async def execute_modify(
-        self,
-        query: Union[str, TextClause],
-        params: Optional[Dict[str, Any]] = None,
-    ) -> bool:
+    async def execute_modify(self, query: str) -> bool:
         """Executa uma query de modificação (INSERT, UPDATE, DELETE)."""
+        if not self.engine:
+            logging.error("Conexão não está aberta. Use 'async with'.")
+
         try:
-            logging.debug('Executando modificação.')
+            logging.debug('Executando modificação assíncrona no PostgreSQL.')
             async with self.engine.begin() as conn:
                 if isinstance(query, TextClause):
-                    await conn.execute(query, params or {})
+                    await conn.execute(query)
                 else:
-                    await conn.execute(text(query), params or {})
+                    await conn.execute(text(query))
             logging.info('Query de modificação executada com sucesso.')
             return True
         except SQLAlchemyError as e:
